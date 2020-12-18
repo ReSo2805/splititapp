ba80c7879ae931ecfaef1b932c87e863efe6e96b
Rene Sommerfeld <rene.sommerfeld@hmmh.de>
2020-12-18 14:35:51 +0100

    SA-5: SpendingPoolActivity anlegen

---
+++ app/src/main/java/de/szut/splitit/activities/SpendingPoolActivity.kt
@@ -0,0 +1,12 @@
+package de.szut.splitit.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import de.szut.splitit.R
+
+class SpendingPoolActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spending_pool)
+    }
+}
