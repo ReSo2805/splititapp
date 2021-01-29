839966151edb2678da097003bc5a1a3bdfd24dbb
Rene Sommerfeld <rene.sommerfeld@hmmh.de>
2021-01-29 10:03:59 +0100

    SA-8: ExpensesPoolActivity: Layout

--- app/src/main/java/de/szut/splitit/activities/ExpensesPoolActivity.kt
+++ app/src/main/java/de/szut/splitit/activities/ExpensesPoolActivity.kt
@@ -2,11 +2,37 @@ package de.szut.splitit.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import de.szut.splitit.R
+import de.szut.splitit.database.views.ExpensesPoolDetails
+import de.szut.splitit.view.ExpensesPoolDetailsRecyclerViewAdapter
 
 class ExpensesPoolActivity : AppCompatActivity() {
+
+    private lateinit var expensesPoolDetailsRecyclerView: RecyclerView
+    private lateinit var expensesPoolDetailsRecyclerViewAdapter: ExpensesPoolDetailsRecyclerViewAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expenses_pool)
+        initializeViews()
     }
+
+    private fun initializeViews() {
+        expensesPoolDetailsRecyclerView = findViewById(R.id.expenses_pool_details_recycler_view)
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        expensesPoolDetailsRecyclerView.layoutManager = layoutManager
+
+        val details =
+                ExpensesPoolDetails("Ausgabenpool 1", 1, 5, 1000f)
+
+        expensesPoolDetailsRecyclerViewAdapter =
+                ExpensesPoolDetailsRecyclerViewAdapter(arrayListOf(details))
+
+        expensesPoolDetailsRecyclerView.adapter = expensesPoolDetailsRecyclerViewAdapter
+    }
+
 }
