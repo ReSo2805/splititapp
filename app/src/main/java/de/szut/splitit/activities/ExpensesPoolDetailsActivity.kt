17ff1a82ad1bb1ea468bfac270cdb77520c110b9
eralmione <eralmi@outlook.de>
2021-02-17 12:05:54 +0100

    SA-23: ExpensesPoolDetailsActivity anlegen

--- app/src/main/java/de/szut/splitit/activities/ExpensesPoolDetailsActivity.kt
+++ app/src/main/java/de/szut/splitit/activities/ExpensesPoolDetailsActivity.kt
@@ -2,6 +2,7 @@ package de.szut.splitit.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import de.szut.splitit.R
 
 class ExpensesPoolDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
